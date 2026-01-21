e43fb59e9278b87cea284018a304b1a97194021d
kenlord24 <156549637+kenlord24@users.noreply.github.com>
2026-01-21 17:37:20 -0500

    updated Robot Container

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -22,7 +22,7 @@ import frc.robot.subsystems.CommandSwerveDrivetrain;
 public class RobotContainer {
     private double MaxSpeed = 1.0 * TunerConstants.kSpeedAt12Volts.in(MetersPerSecond); // kSpeedAt12Volts desired top speed
     private double MaxAngularRate = RotationsPerSecond.of(0.75).in(RadiansPerSecond); // 3/4 of a rotation per second max angular velocity
-    private double NudgeSpeed = 0.3 * MaxSpeed; // 30% of max speed for nudge control
+    private double NudgeSpeed = 0.3 * MaxSpeed; 
 
     /* Setting up bindings for necessary control of the swerve drive platform */
     private final SwerveRequest.FieldCentric drive = new SwerveRequest.FieldCentric()
