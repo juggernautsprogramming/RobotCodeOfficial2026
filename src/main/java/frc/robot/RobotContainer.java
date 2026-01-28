5b4386477fc94701a5bfec870ac07fcf47896bc2
kenlord24 <156549637+kenlord24@users.noreply.github.com>
2026-01-28 17:49:09 -0500

    Added Nudge control, Corrected Direction of swerve Drive

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -22,7 +22,7 @@ import frc.robot.subsystems.CommandSwerveDrivetrain;
 public class RobotContainer {
     private double MaxSpeed = 1.0 * TunerConstants.kSpeedAt12Volts.in(MetersPerSecond); // kSpeedAt12Volts desired top speed
     private double MaxAngularRate = RotationsPerSecond.of(0.75).in(RadiansPerSecond); // 3/4 of a rotation per second max angular velocity
-    private double NudgeSpeed = 0.3 * MaxSpeed; 
+    private double NudgeSpeed = 0.2 * MaxSpeed; 
 
     /* Setting up bindings for necessary control of the swerve drive platform */
     private final SwerveRequest.FieldCentric drive = new SwerveRequest.FieldCentric()
@@ -33,8 +33,8 @@ public class RobotContainer {
 
     private final Telemetry logger = new Telemetry(MaxSpeed);
 
-    private final CommandXboxController joystick = new CommandXboxController(0);
-
+    private final CommandXboxController DriverStick = new CommandXboxController(0);
+    private final CommandXboxController PlayerStick = new CommandXboxController(1);
     public final CommandSwerveDrivetrain drivetrain = TunerConstants.createDrivetrain();
 
     public RobotContainer() {
@@ -47,9 +47,9 @@ public class RobotContainer {
         drivetrain.setDefaultCommand(
             // Drivetrain will execute this command periodically
             drivetrain.applyRequest(() ->
-                drive.withVelocityX(-joystick.getLeftY() * MaxSpeed) // Drive forward with negative Y (forward)
-                    .withVelocityY(-joystick.getLeftX() * MaxSpeed) // Drive left with negative X (left)
-                    .withRotationalRate(-joystick.getRightX() * MaxAngularRate) // Drive counterclockwise with negative X (left)
+                drive.withVelocityX(DriverStick.getLeftY() * MaxSpeed) // Drive forward with negative Y (forward)
+                    .withVelocityY(DriverStick.getLeftX() * MaxSpeed) // Drive left with negative X (left)
+                    .withRotationalRate(-DriverStick.getRightX() * MaxAngularRate) // Drive counterclockwise with negative X (left)
             )
         );
 
@@ -60,22 +60,36 @@ public class RobotContainer {
             drivetrain.applyRequest(() -> idle).ignoringDisable(true)
         );
 
-        joystick.a().whileTrue(drivetrain.applyRequest(() -> brake));
-        joystick.b().whileTrue(drivetrain.applyRequest(() ->
-            point.withModuleDirection(new Rotation2d(-joystick.getLeftY(), -joystick.getLeftX()))
+        DriverStick.a().whileTrue(drivetrain.applyRequest(() -> brake));
+        DriverStick.b().whileTrue(drivetrain.applyRequest(() ->
+            point.withModuleDirection(new Rotation2d(-DriverStick.getLeftY(), -DriverStick.getLeftX()))
         ));
 
         // Run SysId routines when holding back/start and X/Y.
         // Note that each routine should be run exactly once in a single log.
-        joystick.back().and(joystick.y()).whileTrue(drivetrain.sysIdDynamic(Direction.kForward));
-        joystick.back().and(joystick.x()).whileTrue(drivetrain.sysIdDynamic(Direction.kReverse));
-        joystick.start().and(joystick.y()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kForward));
-        joystick.start().and(joystick.x()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kReverse));
+        DriverStick.back().and(DriverStick.y()).whileTrue(drivetrain.sysIdDynamic(Direction.kForward));
+        DriverStick.back().and(DriverStick.x()).whileTrue(drivetrain.sysIdDynamic(Direction.kReverse));
+        DriverStick.start().and(DriverStick.y()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kForward));
+        DriverStick.start().and(DriverStick.x()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kReverse));
 
         // Reset the field-centric heading on left bumper press.
-        joystick.leftBumper().onTrue(drivetrain.runOnce(drivetrain::seedFieldCentric));
+        DriverStick.leftBumper().onTrue(drivetrain.runOnce(drivetrain::seedFieldCentric));
 
         drivetrain.registerTelemetry(logger::telemeterize);
+
+        //Nudge Control
+        DriverStick.povUp().whileTrue(
+            drivetrain.applyRequest(() -> drive.withVelocityX(-NudgeSpeed).withVelocityY(0).withRotationalRate(0))
+        );
+        DriverStick.povDown().whileTrue(
+            drivetrain.applyRequest(() -> drive.withVelocityX(NudgeSpeed).withVelocityY(0).withRotationalRate(0))
+        );
+        DriverStick.povLeft().whileTrue(
+            drivetrain.applyRequest(() -> drive.withVelocityX(0).withVelocityY(-NudgeSpeed).withRotationalRate(0))
+        );
+        DriverStick.povRight().whileTrue(
+            drivetrain.applyRequest(() -> drive.withVelocityX(0).withVelocityY(NudgeSpeed).withRotationalRate(0))
+        );
     }
 
     public Command getAutonomousCommand() {
